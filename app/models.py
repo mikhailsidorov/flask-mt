88d427b2b35f6b0505d412507fcc6057c5433945
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-02-06 12:43:01 +1200

    Chapter 10: Email support

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from hashlib import md5
+from time import  time
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db
-from app import login
+import jwt
+from app import app, db, login
+
 
 
 @login.user_loader
@@ -31,7 +33,7 @@ class User(UserMixin, db.Model):
         secondary=followers, 
         primaryjoin=(followers.c.follower_id == id), 
         secondaryjoin=(followers.c.followed_id == id), 
-        backref=db.backref('followers', lazy='dynamic'), 
+        backref=db.backref('followers', lazy='dynamic'),
         lazy='dynamic'
     )
     
@@ -66,6 +68,20 @@ class User(UserMixin, db.Model):
             filter(followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
 class Post(db.Model):
