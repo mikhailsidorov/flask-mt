dfbd454a483913dfead69ffccd301cdb7abcbd2f
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-02-12 20:33:33 +1200

    Replace old urls

--- app/main/routes.py
+++ app/main/routes.py
@@ -36,8 +36,8 @@ def index():
         return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
     return render_template('index.html',
                            title=_('Home'),
                            form=form,
@@ -52,8 +52,8 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     posts = user.posts.order_by(Post.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) if posts.has_prev else None
+    next_url = url_for('main.user', username=user.username, page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('main.user', username=user.username, page=posts.prev_num) if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
@@ -79,10 +79,10 @@ def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         flash(_('User %(username)s not found.', username=username))
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     if user == current_user:
         flash(_('You cannot follow yourself!'))
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     current_user.follow(user)
     db.session.commit()
     flash(_('You are following %(username)s!', username=username))
