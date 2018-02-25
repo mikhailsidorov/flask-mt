3793c3e8e71e66bbbf49bcb5044f88a3f0c784de
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-02-26 00:54:10 +1200

    Chapter19: Deployment on docker containers

---
+++ boot.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+source venv/bin/activate
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+break fi
+    echo Upgrade command failed, retrying in 5 secs...
+sleep 5 done
+flask translate compile
+exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
