30b5a9d7e46320c017702f462e3b40b8dd50a08c
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-03-01 23:58:46 +1200

    Chapter 20: User notifications

--- app/main/forms.py
+++ app/main/forms.py
@@ -37,3 +37,8 @@ class SearchForm(FlaskForm):
         if 'csrf_enabled' not in kwargs:
             kwargs['csrf_enabled'] = False
         super(SearchForm, self).__init__(*args, **kwargs)
+
+
+class MessageForm(FlaskForm):
+    message = TextAreaField(_l('Message'), validators=[DataRequired(), Length(min=0, max=140)])
+    submit = SubmitField(_l('Submit'))
