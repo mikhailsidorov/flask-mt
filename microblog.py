cf56ec39f6967af75d0fe6d06842c514c46cf0ac
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-03-04 04:21:26 +1200

    Chapter 22: Background jobs

--- microblog.py
+++ microblog.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Post, Message, Notification
+from app.models import User, Post, Message, Notification, Task
 
 
 app = create_app()
@@ -8,7 +8,7 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification, 'Task': Task}
 
 
 if __name__ == '__main__':
