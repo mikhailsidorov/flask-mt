46b12ac44bac7ffaa7010f82d6d5da9d4d8ed0e4
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-04-22 22:26:54 +1200

    Fix create post API issues

--- app/app/authr.py
+++ app/app/authr.py
@@ -1,13 +1,31 @@
 from flask_allows import Allows, Requirement
-from flask import g
+from flask import abort, g
 
+from .api.errors import bad_request
 
 allows = Allows(identity_loader=lambda: g.current_user)
 
 
-class CanUpdateProfile(Requirement):
+class UserRequirementMixin(object):
+    def is_profile_owner(self, identity, request):
+        return identity.id == request.view_args['id']
+
+
+class CanUpdateProfile(UserRequirementMixin, Requirement):
     def fulfill(self, identity, request):
         return self.is_profile_owner(identity, request)
 
     def is_profile_owner(self, identity, request):
         return identity.id == request.view_args['id']
+
+
+class CanCreatePost(UserRequirementMixin, Requirement):
+    def fulfill(self, identity, request):
+        return self.is_profile_owner(identity, request) and \
+               self.is_owner_id_in_data(identity, request)
+
+    def is_owner_id_in_data(self, identity, request):
+        data = request.get_json() or {}
+        if 'user_id' not in data:
+            abort(400, 'must include user_id field')
+        return identity.id == data['user_id']
